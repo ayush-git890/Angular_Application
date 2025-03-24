b563dfa69a382703edcf9864f5023b2833722581
ayush-git890 <ayushofficial4208@gmail.com>
2025-03-24 11:40:56 +0530

    Show error when input name is wrong

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'MyAngularApp';
   message: string = "Hello Ayush! from BridgeLabz";
   logoUrl: string = "assets/bridge-labz.webp";
+  errorMessage: string = ''; 
 
   bridgeLabzUrl: string = "https://www.bridgelabz.com";
 
@@ -16,5 +17,16 @@ export class AppComponent {
     window.open(this.bridgeLabzUrl, "_blank");
   }
 
-  userName: string = '';  // Stores user input
+  validateUserName(): void {
+    const regex = /^[A-Z][a-zA-Z]{2,}$/; 
+    if (this.userName === '') {
+      this.errorMessage = 'Name is required!';
+    } else if (!regex.test(this.userName)) {
+      this.errorMessage = 'Invalid Name! Must start with uppercase & be at least 3 letters.';
+    } else {
+      this.errorMessage = ''; 
+    }
+  }
+
+  userName: string = '';  
 }
