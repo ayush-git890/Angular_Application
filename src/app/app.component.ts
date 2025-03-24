1bec224ad2956940124a7efaae873f2a3c6364e3
ayush-git890 <ayushofficial4208@gmail.com>
2025-03-24 11:16:11 +0530

    display bridgelabz logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'MyAngularApp';
   message: string = "Hello from BridgeLabz";
+  logoUrl: string = "assets/bridge-labz.webp";
 }
